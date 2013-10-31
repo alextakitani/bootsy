da2d744b56d05aa8021d1b43d838a7f5eab1552e
Alex Takitani <atpires@gmail.com>
2013-10-31 09:28:19 -0200

    rails 3 compat

--- lib/bootsy/activerecord/image_gallery.rb
+++ lib/bootsy/activerecord/image_gallery.rb
@@ -2,7 +2,7 @@ module Bootsy
   class ImageGallery < ActiveRecord::Base
     belongs_to :bootsy_resource, polymorphic: true
     has_many :images, dependent: :destroy
-
+    attr_accessible :bootsy_resource_id, :bootsy_resource_type
     scope :destroy_orphans, ->(time_limit) { where('created_at < ? AND bootsy_resource_id IS NULL', time_limit).destroy_all }
   end
 end
